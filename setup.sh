c626d09b3477f847663d9e1a7d1fe4ee9e234e35
z <z@alpine.my.domain>
2023-02-16 15:51:04 +0530

    more

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
 set fish_trace 1
-
+doas apk add neofetch
+neofetch
 # TODO ansible playbook with molecule tests
 
 # change user shell to fish
@@ -11,10 +12,17 @@ chsh -s (which fish)
 setup-desktop gnome
 
 doas apk add git tmux lf micro lazygit
+doas apk add bat fzf btop htop
 
+# basic config
+set -Ux EDITOR micro
+set -U fish_greeting ""
+
+# Add fly-pie
 doas apk add curl file xdg-utils
 curl -L -O -J https://github.com/Schneegans/Fly-Pie/releases/latest/download/flypie@schneegans.github.com.zip
 gnome-extensions install flypie@schneegans.github.com.zip
+gnome-extensions enable flypie@schneegans.github.com
 xdg-mime default firefox.desktop text/html
 
 openrc default
