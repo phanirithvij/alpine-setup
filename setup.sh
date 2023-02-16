09c63b610554b02c9aabc5671b30643d8a493061
z <z@alpine.my.domain>
2023-02-16 15:21:47 +0530

    fix

--- setup.sh
+++ setup.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env fish
 set fish_trace 1
-doas apk add fish shadow
-which fish
-chsh
+
+# TODO ansible playbook with molecule tests
+
+# change user shell to fish
+doas apk add shadow
+chsh -s (which fish)
+
 # TODO setup awesome
 setup-desktop gnome
+
 doas apk add git tmux lf micro lazygit
-doas apk add curl file
-curl -O -J https://github.com/Schneegans/Fly-Pie/releases/latest/download/flypie@schneegans.github.com.zip
+
+doas apk add curl file xdg-utils
+curl -L -O -J https://github.com/Schneegans/Fly-Pie/releases/latest/download/flypie@schneegans.github.com.zip
 gnome-extensions install flypie@schneegans.github.com.zip
+xdg-mime default firefox.desktop text/html
+
 openrc default
