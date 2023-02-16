22014e4fbc0ff11cb1a001193fb076e555ffc81c
Phani Rithvij <phanirithvij2000@gmail.com>
2023-02-16 14:12:02 +0530

    push

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ setup-alpine replacement for personal use (won't be maintained)
 ## Before running this
 
 - Run `setup-alpine && apk add curl fish`
-- `curl http://lnkiy.in/alpset | fish`
+- `curl -L http://lnkiy.in/alpset | fish`
--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ doas apk add git tmux lf micro lazygit
 doas apk add curl file
 curl -O -J https://github.com/Schneegans/Fly-Pie/releases/latest/download/flypie@schneegans.github.com.zip
 gnome-extensions install flypie@schneegans.github.com.zip
-rc default
+openrc default
