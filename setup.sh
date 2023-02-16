f9ebc586969e0a5802ceb7da184b03d2cf081d2d
z <z@alpine.my.domain>
2023-02-16 22:03:24 +0530

    remove doas

--- setup.sh
+++ setup.sh
@@ -1,30 +1,32 @@
 #!/usr/bin/env fish
 set fish_trace 1
-doas apk add neofetch
-neofetch
+apk add neofetch
+neofetch | tee ~/neofetch-fresh.log
 
 # TODO ansible playbook with molecule tests
 # TODO dotfiles (yadm, chezmoi, stow, dotbot, some new tui)
 
 # change user shell to fish
-doas apk add shadow
+apk add shadow
 chsh -s (which fish)
 
 # TODO setup awesome
 setup-desktop gnome
 
-doas apk add git tmux lf micro lazygit
-doas apk add bat fzf btop htop
+apk add git tmux lf micro lazygit
+apk add bat fzf btop htop
 
 # basic config
 set -Ux EDITOR micro
 set -U fish_greeting ""
 
 # Add fly-pie
-doas apk add curl file xdg-utils
+apk add curl file xdg-utils
 curl -L -O -J https://github.com/Schneegans/Fly-Pie/releases/latest/download/flypie@schneegans.github.com.zip
 gnome-extensions install flypie@schneegans.github.com.zip
+rm flypie@schneegans.github.com.zip
 gnome-extensions enable flypie@schneegans.github.com
 xdg-mime default firefox.desktop text/html
 
+neofetch | tee ~/neofetch-gnome.log
 openrc default
